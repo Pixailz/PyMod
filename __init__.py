2a33408f01fa7d033e928a08b0b14b32b7c8a75f
Pixailz <Pixailz1@gmail.com>
2021-01-19 00:50:40 +0100

    💎  : 1.07.1

--- __init__.py
+++ __init__.py
@@ -1,10 +1,11 @@
-# Version : 1.07.0
+# Version : 1.07.1
 """
 TODO
     - change port open discover
     from nmaplib to socket lib (port_scan)
     - review isup func (utils)
 """
+
 __all__ = [
     "pentest",
     "utils"
